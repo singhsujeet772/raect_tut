9cbd9883beefe11157e8f54c65e9ae3e99f870b2
singhsujeet772 <singhsujeet772@gmail.com>
2023-12-27 12:12:58 +0530

    Get Input Box value

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 import Firstcomponent from './Firstcomponent.js';
 import Studentprops from './Studentprops.js';
 
 
 function App() {
+  const [data,setData] = useState('Narendra');
+  const [val,setVal] = useState(null);
+  const [print,setPrint] = useState(false);
+  function getInputData(val){
+    setVal(val.target.value)
+    setPrint(false)
+  }
   return (
     <div className="App">
       {/* <header className="App-header">
@@ -21,8 +29,16 @@ function App() {
           Learn React
         </a>
       </header> */}
+      {
+        print ? <h3>{val}</h3> : null
+      }
+      <button onClick={()=>setPrint(true)}>Print Data</button>
+      <h2>{val}</h2>
+      <input type='text' onChange={getInputData}/>
+      <h1>{data}</h1>
       <Firstcomponent />
       <Studentprops name={"sujeet"} email={"sujeet@mail.com"} other={{address:"delhi",phone:"8978678"}}/>
+      <button onClick={()=>setData("Modi")}>Change Name</button>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores quas non numquam iste nam enim culpa, aliquid error, neque quis dolore. Eveniet unde veritatis quisquam est earum repudiandae tempore enim?</p>
     </div>
   );
